53401627aaf0d027c4283f4c7b566ee6d519d5ce
wangxy <1948288473@qq.com>
2023-12-11 10:42:03 +0800

    :fire: 优化自定义converter注册方式

--- backend/src/main/java/cn/linter/light/config/JdbcConfig.java
+++ backend/src/main/java/cn/linter/light/config/JdbcConfig.java
@@ -2,11 +2,10 @@ package cn.linter.light.config;
 
 import cn.linter.light.converter.BooleanToIntegerConverter;
 import cn.linter.light.converter.IntegerToBooleanConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.lang.NonNull;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,15 +18,13 @@ import java.util.List;
 @Configuration
 public class JdbcConfig extends AbstractJdbcConfiguration {
 
-    @Bean
+    @NonNull
     @Override
-    @SuppressWarnings("NullableProblems")
-    public JdbcCustomConversions jdbcCustomConversions() {
-        List<Converter<?, ?>> converterList = Arrays.asList(
+    protected List<?> userConverters() {
+        return Arrays.<Converter<?, ?>>asList(
                 new BooleanToIntegerConverter(),
                 new IntegerToBooleanConverter()
         );
-        return new JdbcCustomConversions(converterList);
     }
 
 }
