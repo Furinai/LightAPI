9093934e8463b85e4dc2852c8fafbbe03205cfe6
wangxy <1948288473@qq.com>
2023-12-11 10:38:09 +0800

    :sparkles: 完善测试接口页面

--- frontend/src/views/ApiConfigTest.vue
+++ frontend/src/views/ApiConfigTest.vue
@@ -1,11 +1,30 @@
 <template>
     <el-form label-position="top">
-        <el-form-item label="URL">
-            <el-input v-model="completeRequestPath">
+        <el-form-item label="URL:">
+            <el-input v-model="completeRequestPath" disabled>
                 <template #append>
                     <el-button @click="copyToBoard">复制</el-button>
                 </template>
             </el-input>
+        </el-form-item>
+        <el-form-item label="请求参数:">
+            <el-form-item v-for="param in apiConfig.paramConfigList" :label="param.name">
+                <el-date-picker v-if="param.type==='日期时间'" v-model="param.value"
+                                type="datetime" value-format="YYYY-MM-DD HH:mm:ss"/>
+                <el-input v-else v-model="param.value"/>
+            </el-form-item>
+            <el-form-item v-if:="apiConfig.pageable" label="PageNum">
+                <el-input v-model="requestParam.pageNumber"/>
+            </el-form-item>
+            <el-form-item v-if:="apiConfig.pageable" label="PageSize">
+                <el-input v-model="requestParam.pageSize"/>
+            </el-form-item>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="testApi">测试</el-button>
+        </el-form-item>
+        <el-form-item v-show="responseContent !== ''" label="响应内容">
+            <el-input type="textarea" v-model="responseContent" :autosize="{minRows: 6}"/>
         </el-form-item>
     </el-form>
 </template>
@@ -17,8 +36,12 @@ export default {
     name: 'ApiConfigTest',
     data() {
         return {
+            apiConfig: {},
             completeRequestPath: '',
-            apiConfig: {}
+            requestParam: {},
+            responseContent: '',
+            responseStatus: '',
+            responseStatusText: ''
         }
     },
     created() {
@@ -31,16 +54,30 @@ export default {
                     response.json().then(data => {
                         this.apiConfig = data
                         let baseUrl = import.meta.env.VITE_BASE_API_URL
-                        this.completeRequestPath = baseUrl + this.apiConfig.requestPath
+                        this.completeRequestPath = baseUrl + data.requestPath
                     })
                 }
             })
         },
-        testApi() {
-            let config = {method: this.apiConfig.requestMethod}
+        testApi: function () {
+            this.apiConfig.paramConfigList.map(param => {
+                this.requestParam[param.key] = param.value
+            })
+            let requestMethod = this.apiConfig.requestMethod
+            let config = {method: requestMethod}
+            if (requestMethod === 'GET' || requestMethod === 'DELETE') {
+                config.param = this.requestParam
+            }
+            if (requestMethod === 'POST' || requestMethod === 'PUT') {
+                config.body = this.requestParam
+            }
             request(this.apiConfig.requestPath, config).then(response => {
                 if (response.ok) {
-                    response.json().then(data => {
+                    this.$message.success('OK')
+                    response.text().then(data => {
+                        if (data != null && data !== '') {
+                            this.responseContent = JSON.stringify(JSON.parse(data), null, 4)
+                        }
                     })
                 }
             })
